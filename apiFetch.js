0893af8c42794e0ef58bb4ff1a851baad4388869
andrewdmo <andrew.olivera@hotmail.com>
2022-02-10 14:21:55 -0300

    search bar

--- apiFetch.js
+++ apiFetch.js
@@ -1,6 +1,6 @@
 const nameTxt = window.document.querySelector("h2#name")
 const genderTxt = document.querySelector("h2#gender")
-const divInfo = document.querySelector("div#info")
+const divInfo = document.querySelector("#info")
 const getPersonUrl = id => `https://swapi.dev/api/people/${id}/`
 const personsPromises = []
 
@@ -12,19 +12,50 @@ for(let i=1;i<82;i++){
 
 Promise.all(personsPromises)
   .then(persons => {
-    console.log(persons);
-
+    // console.log(persons);
+    input = document.getElementById("pesquisa")
     const lisPersons = persons.reduce((accumulator,person,index) =>{
       accumulator += `<div>
                         <img class="imagens" src= https://starwars-visualguide.com/assets/img/characters/${index + 1}.jpg>
-                        <h3 class= "card"> Name: ${person.name}</h3>
-                        <p class="card-subtitle"> Gender: ${person.gender}</p>
+                        <h3 class= "card">${person.name}</h3>
+                        <p class="card-subtitle">${person.gender}</p>
                         <p>${person.url}</p>
                       </div>`
                       
       return divInfo.innerHTML = accumulator
     },'')
-    console.log(lisPersons);
+    // console.log(lisPersons);
   })
-  // nameTxt.innerHTML = `Name: ${json.name}`
-  // genderTxt.innerHTML = `Gender: ${json.gender}`
+
+function pesquisa() {
+
+  var input, filter,divs
+
+  input = document.getElementById("pesquisa")
+  divs = document.getElementsByTagName("div")
+  filter = input.value.toUpperCase();
+
+  for (let i = 0; i < divs.length; i++) {
+
+    div = divs[i].getElementsByTagName("h3")[0]
+
+    textValue = div.textContent || div.innerText;
+    textValue = textValue.toUpperCase()
+
+    console.log(filter);
+
+    if(textValue.indexOf(filter) > -1){
+
+      divs[i].style.display = ""
+      console.log(textValue + " Deu boa");
+
+    }else{
+
+      divs[i].style.display = "none"
+      console.log(textValue + " Deu ruim");
+
+    }
+    
+  }
+
+}
